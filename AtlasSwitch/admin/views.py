060b9ecf46eed523c36a84b7b4098c1e87e42a8b
jp117 <john.paradise117@gmail.com>
2019-01-15 10:29:11 -0500

    Built out admin control panel skeleton, need to add forms and functionality

--- AtlasSwitch/admin/views.py
+++ AtlasSwitch/admin/views.py
@@ -13,3 +13,38 @@ admin = Blueprint('admin', __name__)
 @admin_required
 def admin_index():
     return render_template('admin/index.html')
+
+
+@admin.route('/admin/report_panel')
+@login_required
+@admin_required
+def report_panel():
+    return render_template('admin/report_panel.html')
+
+
+@admin.route('/admin/user_panel')
+@login_required
+@admin_required
+def user_panel():
+    return render_template('admin/user_panel.html')
+
+
+@admin.route('/admin/portfolio_panel')
+@login_required
+@admin_required
+def portfolio_panel():
+    return render_template('admin/portfolio_panel.html')
+
+
+@admin.route('/admin/blog_panel')
+@login_required
+@admin_required
+def blog_panel():
+    return render_template('admin/blog_panel.html')
+
+
+@admin.route('/admin/static_panel')
+@login_required
+@admin_required
+def static_panel():
+    return render_template('admin/static_panel.html')
